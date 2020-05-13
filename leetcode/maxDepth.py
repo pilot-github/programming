91e6ef4671db9185460661b565528c137143c80d
durgadrahul <durgad.rahul@gmail.com>
2020-05-13 19:43:46 -0400

    Create maxDepth.py

---
+++ leetcode/maxDepth.py
@@ -0,0 +1,19 @@
+def maxDepth1(self, root: TreeNode) -> int:
+    depth = 0
+    left_depth = 0
+    right_depth = 0
+    if root and root.val is not None:
+        depth = depth + 1
+        if root.left:
+            left_depth = self.maxDepth(root.left)
+        if root.right:
+            right_depth = self.maxDepth(root.right)
+
+        if left_depth > right_depth:
+            depth += left_depth
+        else:
+            depth += right_depth
+    return depth
+    
+def maxDepth2(self, root):
+    return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) if root else 0
