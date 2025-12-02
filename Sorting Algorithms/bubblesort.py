27bb9010dd51ed92620dd2f0c8631f843d4d4fb7
pilot-github <durgad.rahul@gmail.com>
2025-12-02 04:51:26 -0700

    Update bubblesort.py - Added swap optimization

--- Sorting Algorithms/bubblesort.py
+++ Sorting Algorithms/bubblesort.py
@@ -2,13 +2,16 @@ def bubble_sort(num):
     print(num)
     l = len(num)
     for i in range(l):
+        swapped = False
         for j in range(l-1-i): ## (l-i-1) is more efficient than (i-1) since last i elements are already sorted
             if num[j] > num[j+1]:
                 num[j], num[j+1] = num[j+1], num[j]
+                swapped = True
         print(num)
     return num
 
 num = [19,2,31,45,6,11,121,27]
 
 print (bubble_sort(num))
+
 
