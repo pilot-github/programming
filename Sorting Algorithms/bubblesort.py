7bf1c6a203055a8bfabbbc4888ea2076f1f919af
pilot-github <durgad.rahul@gmail.com>
2025-12-02 04:52:47 -0700

    Update bubblesort.py

--- Sorting Algorithms/bubblesort.py
+++ Sorting Algorithms/bubblesort.py
@@ -7,11 +7,15 @@ def bubble_sort(num):
             if num[j] > num[j+1]:
                 num[j], num[j+1] = num[j+1], num[j]
                 swapped = True
+
+        if not swapped:
+            break
         print(num)
     return num
 
 num = [19,2,31,45,6,11,121,27]
 
 print (bubble_sort(num))
+
 
 
