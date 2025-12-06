38721c74f0c9806ad9fbc58a17b82f5b40cd7bc7
pilot-github <durgad.rahul@gmail.com>
2025-12-05 21:24:20 -0700

    Update selection_sort.py

--- Sorting Algorithms/selection_sort.py
+++ Sorting Algorithms/selection_sort.py
@@ -1,11 +1,11 @@
 def selection_sort(num):
     for i in range(len(num)):
         min_index = i
-        print(f"Min Index: {min_index}")
         for j in range(min_index+1, len(num)):
-            if num[min_index] > num[j]:
+            if num[j] < num[min_index]:
                 min_index = j
         
+        print(f"Min Index: {min_index}, Value: {num[min_index]}")
         num[i], num[min_index] = num[min_index], num[i]
         print(num)
     return num
@@ -15,3 +15,4 @@ num = [19,2,31,45,6,11,121,27]
 print (f"Original Input Array : {num}")
 selection_sort(num)
 print (f"Final Output : {num}")
+
