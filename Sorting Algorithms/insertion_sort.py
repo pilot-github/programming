feafb6db9874e5d83b2b8f85e83ba0e9ee26df0e
pilot-github <durgad.rahul@gmail.com>
2025-12-05 20:24:25 -0700

    Update insertion_sort.py

--- Sorting Algorithms/insertion_sort.py
+++ Sorting Algorithms/insertion_sort.py
@@ -3,13 +3,14 @@ def insertion_sort(num):
         next = num[i]
         j = i-1
 
-        while num[j] > next and j>=0:
+        while next < num[j] and j >= 0:
             num[j+1] = num[j]
             j = j-1
-
+            print(num)
         num[j+1] = next
 
     return num
 
 num = [19,2,31,45,6,11,121,27]
+
 print (insertion_sort(num))
