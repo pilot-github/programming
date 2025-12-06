66fc66c7d329d734f540d0f86376e08ba2e34917
pilot-github <durgad.rahul@gmail.com>
2025-12-05 20:31:00 -0700

    Update insertion_sort.py

--- Sorting Algorithms/insertion_sort.py
+++ Sorting Algorithms/insertion_sort.py
@@ -7,7 +7,7 @@ def insertion_sort(num):
         while key < num[j] and j >= 0:
             num[j+1] = num[j]
             j = j-1
-            print(num)
+        print(num)
         num[j+1] = key
 
     return num
@@ -16,5 +16,6 @@ num = [19,2,31,45,6,11,121,27]
 print(f"Original Input Array : {num}")
 insertion_sort(num)
 print(f"Final Output Array : {num}")
+
 
 
