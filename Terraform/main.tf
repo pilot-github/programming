f2a22a95a2ae7fbd76ae858c073f3a96e378e1bb
pilot-github <durgad.rahul@gmail.com>
2025-12-10 20:09:41 -0700

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 # Define an S3 bucket resource
 resource "aws_s3_bucket" "example_bucket" {
   # Bucket names must be globally unique
-  bucket = pilot-s3-bucket"
+  bucket = "pilot-s3-bucket"
 
   tags = {
     Name        = "Managed by Terraform"
